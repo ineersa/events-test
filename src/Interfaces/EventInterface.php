434a77925fb58b38d91e820c84e022a36cc3a777
ineersa <i.vasylevskyi@theappsolutions.com>
2019-10-30 20:32:38 +0200

    structure

---
+++ src/Interfaces/EventInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Interfaces;
+
+interface EventInterface
+{
+    public function getDataObject();
+}
---
+++ src/Interfaces/SubscriberInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Interfaces;
+
+interface SubscriberInterface
+{
+    public function getSubscribedEvents() : array;
+}
